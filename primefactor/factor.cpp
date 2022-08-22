199e66f3345d8ad2e3ef7178fde56df5c6e6e3e1
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-22 10:01:44 +0530

    prime factor

---
+++ primefactor/factor.cpp
@@ -0,0 +1,36 @@
+/*
+2 18
+5 18
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void primefactor(long long n)
+{
+    int count = 0;
+    while (!(n % 2))
+    {
+        n >>= 1;
+        count++;
+    }
+    if (count)
+        cout << 2 << "  " << count << endl;
+    for (long long i = 3; i <= sqrt(n); i += 2)
+    {
+        count = 0;
+        while (n % i == 0)
+        {
+            count++;
+            n = n / i;
+        }
+        if (count)
+            cout << i << "  " << count << endl;
+    }
+    if (n > 2)
+        cout << n << "  " << 1 << endl;
+}
+int main()
+{
+    long long n = 1000000000000000000;
+    primefactor(n);
+    return 0;
+}
