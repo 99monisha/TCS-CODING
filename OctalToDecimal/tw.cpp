449ca057ad78cb5a6534f3d69ec59402d916294f
Monisha Dolai <monishadolai1998@gmail.com>
2022-11-05 22:24:24 +0530

    jhjj

---
+++ OctalToDecimal/tw.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    string binaryNumber;
+    cin >> binaryNumber;
+    int base = 8;
+    cout << stoi(binaryNumber, 0, base);
+    return 0;
+}
