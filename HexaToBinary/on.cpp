c6a59966e7cc45d34ac5d0570c041c3c8ebcb0ef
Monisha Dolai <monishadolai1998@gmail.com>
2022-11-06 20:37:38 +0530

    nnnmggh

---
+++ HexaToBinary/on.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int convert(string num)
+{
+    int len = num.size();
+    int dec = 0, index = 0;
+    for(int i = len - 1; i >= 0; i--)
+    {
+        if (num[i] >= '0' && num[i] <= '9') 
+        {
+            int digit = int(num[i]) - 48; 
+            dec += digit * pow(16, index); 
+            index++; 
+        } 
+        else if (num[i] >= 'A' && num[i] <= 'F') 
+        { 
+            int digit = int(num[i]) - 55; 
+            dec += digit * pow(16, index); 
+            index++; 
+        } 
+    } 
+    return dec; 
+} 
+int main() 
+{ 
+    string num; 
+    cin >> num;
+    cout << (convert(num));
+    return 0;
+}
